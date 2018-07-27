2b13f131c317be8e9d42b5cf02c125f4064de759
hunterlr <hongshouzh@163.com>
2018-07-27 11:14:33 +0800

    简项源，更新港澳台返回

--- application/libraries/Jiaoke.php
+++ application/libraries/Jiaoke.php
@@ -50,6 +50,52 @@ class Jiaoke {
         }
     }
 
+    function getIDcheckNew($name,$idNo)
+    {
+        try
+        {
+            $appid = "anhuixinhua1";
+            $appkey = "b0d51906bb8d52d7d36036908e4afbc9";
+            $apiurl = "idcheck";
+            $sign = md5($idNo.$name.$appkey);
+
+            $arr = array(
+                'Appid'=>$appid,
+                'Name'=>$name,
+                'IdCode'=>$idNo,
+                'Sign'=>$sign
+            );
+            $data_string =  json_encode($arr,JSON_UNESCAPED_UNICODE);
+            //log_message('info',"111");
+            //print_r($data_string);
+            $ch = curl_init();
+            curl_setopt($ch,CURLOPT_URL,$this->url.$apiurl);
+            curl_setopt($ch, CURLOPT_HEADER, false);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST,true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS,$data_string);
+            curl_setopt($ch, CURLOPT_TIMEOUT,5);
+            curl_setopt($ch, CURLOPT_HTTPHEADER,array(
+                'Content-Type: application/json; charset=utf-8',
+            ));
+            $out = curl_exec($ch);
+            if(curl_errno($ch))
+            {
+                log_message('error',$data_string."---curlerr---time:".date("Y-m-d H:i:s"));
+                return "500";
+            }
+            curl_close($ch);
+            log_message('info','source返回:'.$out);
+            return $out;
+        }
+        catch(Exception $e)
+        {
+            log_message('error',$data_string."---ex---time:".date("Y-m-d H:i:s"));
+            log_message('error',$e->getMessage());
+            return "500";
+        }
+    }
+
     function mobile($name,$idNo,$phone)
     {
         try
