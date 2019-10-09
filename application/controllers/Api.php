7b2595456cc371b940b40a00a80fe17c687e9b43
hunterlr <hongshouzh@163.com>
2019-10-09 13:39:14 +0800

    update

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1398,7 +1398,7 @@ class Api extends CI_Controller {
     }
 
 
-    public function mobiletatus()
+    public function mobilestatus()
     {
         try{
             //判断用户接口权限
@@ -2293,6 +2293,115 @@ class Api extends CI_Controller {
                             $code = "100";
                             $orderno = $this->apiclass->createorderno();
                             $this->apiclass->updatedb($validitycode["userproid"],$validitycode["userid"],$validitycode["proid"],$datajson,$state,$ischarge,$orderno,"jiaokebankthree");
+                            $this->apiclass->response($code,$result,$orderno);
+                        }
+                        else
+                        {
+                            $this->apiclass->response(500);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                $this->apiclass->response($code);
+            }
+        }
+        catch (Exception $e)
+        {
+            log_message('error',$e->getMessage());
+            $this->apiclass->response(500);
+        }
+    }
+
+    public function banktwo(){
+        try{
+            //判断用户接口权限
+            $validitycode = $this->apiclass->validate();
+            $code = is_numeric($validitycode)?$validitycode:1;
+            $code = 1;
+            if($code == 1)
+            {
+                $datajson = file_get_contents('php://input');
+                log_message('info',$datajson."---time:".date("Y-m-d H:i:s"));
+                $datajson = $this->apiclass->decrypt($datajson);
+                log_message('info',$datajson."---time:".date("Y-m-d H:i:s"));
+                $data = json_decode($datajson,true);
+                $name = !empty($data["name"])?$data["name"]:null;
+                $bankcard = !empty($data["bankcard"])?$data["bankcard"]:null;
+                //$phone = !empty($data["phone"])?$data["phone"]:null;
+                //判断参数
+                if($name == null || $bankcard == null)
+                {
+                    $code = 110;
+                    $this->apiclass->response($code);
+                }
+                else
+                {
+                    $data = array(
+                        "name"=>$name,
+                        "bank_card_number"=>$bankcard
+                    );
+
+                    $this->load->library('jiaokenew');
+                    $out = $this->jiaokenew->getdata("banktwoinfo",$data);
+                    //判断返回值
+                    if($out == "500")
+                    {
+                        $this->apiclass->response($out);
+                    }
+                    else
+                    {
+                        $arr = json_decode($out,true);
+                        $code = !empty($arr["code"])?$arr["code"]:null;
+                        //var_dump($arr);
+                        //判断返回json
+                        if ($code)
+                        {
+
+                            $result = "";
+                            //$state = $result["state"];
+                            //var_dump($result);
+                            $ischarge = 0;
+                            switch ($arr["code"])
+                            {
+                                case  "2000":
+                                    $state = "1100";
+                                    $result = array(
+                                        "result"=>"一致",
+                                        "state"=>$state
+                                    );
+                                    $ischarge = 1;
+                                    break;
+                                case  "2001":
+                                    $state = "1101";
+                                    $result = array(
+                                        "result"=>"不一致",
+                                        "state"=>$state
+                                    );
+                                    $ischarge = 1;
+                                    break;
+                                case  "2002":
+                                case  "2003":
+                                case  "2004":
+                                case  "2005":
+                                case  "2006":
+                                case  "2007":
+                                case  "2008":
+                                    $state = "1102";
+                                    $result = array(
+                                        "result"=>"库中无此号",
+                                        "state"=>$state
+                                    );
+                                    $ischarge = 0;
+                                    break;
+                                default:
+                                    $this->apiclass->response(500);
+                                    return;
+                            }
+                            $code = "100";
+                            $orderno = $this->apiclass->createorderno();
+                            $this->apiclass->updatedb($validitycode["userproid"],$validitycode["userid"],$validitycode["proid"],$datajson,$state,$ischarge,$orderno,"jiaokebanktwoinfo");
                             $this->apiclass->response($code,$result,$orderno);
                         }
                         else
