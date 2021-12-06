f6979325a5de350a29a1008bd77a3587002a282a
unknown <hongshouzh@163.com>
2021-12-06 10:29:43 +0800

    update

--- application/libraries/Xiaoshi.php
+++ application/libraries/Xiaoshi.php
@@ -34,7 +34,63 @@ class Xiaoshi {
         log_message('info',$alldatastr);
         $ch = curl_init();
 
-        curl_setopt($ch,CURLOPT_URL,$postdata->url);
+        curl_setopt($ch,CURLOPT_URL,$postdata->url3);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST,true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$alldatastr);
+        curl_setopt($ch, CURLOPT_TIMEOUT,$time);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,array(
+            'Content-Type: application/x-www-form-urlencoded;charset=utf-8,mvTrackId:'.$mvTrackId
+        ));
+        $out = curl_exec($ch);
+
+        if(curl_errno($ch))
+        {
+            log_message('error',$datastr."---curlerr---time:".date("Y-m-d H:i:s").$ch);
+            return "500";
+        }
+        curl_close($ch);
+        log_message('info','source返回:'.$out);
+        $jsonout = json_decode($out,true);
+
+        return $out;
+    }
+
+
+    function bankfour($name,$idNo,$bankcard,$phone,$time = 5){
+        $postdata = new postdata();
+        $arr = array(
+            'name' => $name,
+            'idCard' => $idNo,
+            'accountNo' => $bankcard,
+            'mobile' => $phone
+        );
+
+        $datastr = json_encode($arr, JSON_UNESCAPED_UNICODE);
+        log_message('info',$datastr);
+
+        $alldata = array(
+            "loginName" => $postdata->account,
+            "pwd" => $postdata->key,
+            "serviceName" => $postdata->servicename,
+            "param" => $arr
+        );
+
+        $randStr = str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890');
+        $rand = substr($randStr,0,6);
+
+        $mvTrackId = date("YmdHis")."_".$postdata->servicename."_".$postdata->account.$rand;
+
+
+        $alldatastr = json_encode($alldata, JSON_UNESCAPED_UNICODE);
+
+        log_message('info',$alldatastr);
+        $ch = curl_init();
+
+        curl_setopt($ch,CURLOPT_URL,$postdata->url4);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST,true);
@@ -74,5 +130,6 @@ class postdata{
     public $account = "huasheng"; //账号
     public $key = "huasheng0225";//密码
     public $servicename = "NameIDCardAccountVerify";
-    public $url = "https://www.miniscores.cn:8313/CreditFunc/v2.1/NameIDCardAccountVerify";// 测试地址
+    public $url3 = "https://www.miniscores.cn:8313/CreditFunc/v2.1/NameIDCardAccountVerify";// 测试地址
+    public $url4 = "https://www.miniscores.cn:8313/CreditFunc/v2.1/NameIDCardAccountVerify";// 测试地址
 }
